61077c91bd271368b78d8e2538b9e35cf9b1956d
Marylange <marylangess@gmail.com>
2020-03-28 14:48:15 -0300

    Atividades Higher Order Functions

--- exercise8.js
+++ exercise8.js
@@ -62,11 +62,34 @@ const books = [
     releaseYear: 1928,
   },
 ];
-// Faça uma função que retorne true se algum livro foi lançado na década de 80 e false caso contrário.
-const expected_result = true
 
-const someBookWasReleaseOnThe80s = (bookLaunch) => {
-  return Object.values(bookLaunch).some((book) => book.releaseYear > 1979 && book.releaseYear < 1990);
+const expected_result = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954
+  }
+]
+// Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene - o pelo livro mais velho.
+function oldBooks(livro) {
+  const livrosOrdenados = Object.values(livro).filter((book) => book.releaseYear <= 1960);
+  return livrosOrdenados.sort((a,b) => a.releaseYear - b.releaseYear);
 }
-console.log(someBookWasReleaseOnThe80s(books));
-assert.equal(someBookWasReleaseOnThe80s(books), expected_result);
+console.log(oldBooks(books))
+assert.deepEqual(oldBooks(books), expected_result);
